59d27962b027cd8fcff18479931a3412c18ded51
Milo <jchernandez10ocm@gmail.com>
2026-05-22 11:21:41 -0500

    chore: add project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>POS Tiquetes — Cosmos</h1>
+      <p>Portal de taquilla funcionando</p>
+      <p>prueba</p>
+    </div>
+  )
+}
+
+export default App
